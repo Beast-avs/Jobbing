b755c5b6da1d43d619ce7a3f69fc373ce669b9fc
Beast <beast.avs@gmail.com>
2013-08-23 01:25:46 +0300

    Modified program 'SNMP trap sender'. Feature sending clear messages was added. Documentation updated (clear messages sending feature was described).

--- SNMP_trapsender/SNMP_trapsender.pl
+++ SNMP_trapsender/SNMP_trapsender.pl
@@ -1,19 +1,24 @@
 #!/usr/bin/perl -w
 
 #
-# This script sends SNMP messages generated as trap to NMS host.
+# This script sends SNMP messages generated as trap or clear to NMS host.
 # This script has a configuration file which contains all necessary 
 # values, such as:
 #      - A list of SNMP_service instances (hosts);
 #      - A list of metrics (counters) for all SNMP_service instances;
 #      - A list of SNMP configuration parameters (community, version);
 #      - IP adress of the host which receives SNMP traps (NMS);
-#      - Pathes to log and lock files.
+#      - Paths to log, lock, and history files.
 # 
 # The script has a mechanism which not allows to run more than one its 
 # instance simultaneously. 
 #
 # Log file contains the results of key calls and steps during the work.
+#
+# For sending the clear messages (signal that problem has been solved) 
+# the history file is used. It keeps the pairs Service_name-Metric_name. As
+# soon as problem is found this pair adds to history file. When problem has
+# solved this pair is removed from history.
 #
 # The path to configuration file is hardcoded.
 #
@@ -27,7 +32,6 @@ use strict;
 use POSIX;
 use File::Basename;
 use Getopt::Long qw(:config no_ignore_case bundling);
-use Sys::Hostname;
 use FileHandle;
 
 our $PROGRAMM_VERSION = "1.0";
@@ -46,6 +50,7 @@ our $METRICS_EXTRACTOR = "/<PATH_TO_METRIC_EXTRACTOR>/METRICS_EXTRACTOR.sh";
 # Read from config file
 our $LOG_FILE = "";
 our $LOCK_FILE = "";
+our $HIST_FILE = "";
 our $NMS_host = "";
 our %Snmp_service;
 our %Metrics;
@@ -65,7 +70,7 @@ GetOptions(
 
 sub Help (){
 	print << "HELP";
-This script sends SNMP messages generated as trap to NMS host.
+This script sends SNMP messages generated as trap or clear to NMS host.
 
 Version: $PROGRAMM_VERSION
 
@@ -136,7 +141,9 @@ sub ConfigurationRead($){
 				($LOG_FILE) = $conf_line =~ /"([^"]*)"/;
 			}elsif($conf_line =~ /^Lock_file/){
 				($LOCK_FILE) = $conf_line =~ /"([^"]*)"/;
-			}elsif($conf_line =~ /^NMS_host/){
+			}elsif($conf_line =~ /^History_file/){
+				($HIST_FILE) = $conf_line =~ /"([^"]*)"/;
+            }elsif($conf_line =~ /^NMS_host/){
 				($NMS_host) = $conf_line =~ /"([^"]*)"/;
 			}elsif($conf_line =~ /^SNMP\s*=\s*{/){
 				$snmp_flag = 1;
@@ -176,6 +183,9 @@ sub CheckConfiguration(){
 	if (!defined($LOG_FILE) || $LOG_FILE eq ""){
 		Errors("ERROR", "Path to log file ($LOG_FILE) is not set.\n");
 	}
+	if (!defined($HIST_FILE) || $HIST_FILE eq ""){
+        Errors("ERROR", "Path to history file ($HIST_FILE) is not set.\n");
+    }
 	if ($NMS_host !~ /\b(\d{1,3}(?:\.\d{1,3}){3})\b/){
 		Errors("ERROR", "IP address of NMS is not set or unvalid ($NMS_host).\n");
 	}
@@ -198,68 +208,96 @@ sub CheckConfiguration(){
 	}
 }
 
-sub CheckSnmpserviceInstanceAvailability ($) {
-	my %Snmp_service_instance = %{shift @_};
-	my $result = 0;
+sub isClearTrapNeeded ($){
+    my $record = shift @_;
+    my $result = 0;
+    my @HISTORY = ();
+    
+    if (-f $HIST_FILE){
+       open (IN_FILE, "<$HIST_FILE") or Errors("ERROR", "Unable to open history file $HIST_FILE");
+       @HISTORY = <IN_FILE>;
+       close (IN_FILE);
+    }
 
-	Errors("OK", "Check availability of $Snmp_service_instance{'name'}");
+    open (IN_FILE, ">$HIST_FILE") or Errors("ERROR", "Unable to open history file $HIST_FILE. It seems to blocked.");
+    foreach my $file_record (@HISTORY){
+       chomp($file_record);
+       ($file_record eq $record) ? $result = 1 : print IN_FILE "$file_record\n";
+    }
+    close (IN_FILE);
 
-	if (!-f $METRICS_EXTRACTOR){
-		Errors("ERROR", "Could not find $METRICS_EXTRACTOR.");
-	}
-
-	$result = (split(/=/,`$METRICS_EXTRACTOR $Snmp_service_instance{'host'} -s Uptime`))[1];
-
-	if ("" eq $result or !$result){
-		SendSNMPTRAP("Snmp service instance $Snmp_service_instance{'name'} ($Snmp_service_instance{'host'}) is unavailable.");
-	} else {
-		$result =~ s/^\s*//;
-		$result =~ s/\s*$//;
-		Errors("OK", "Snmp service instance $Snmp_service_instance{'name'} is working. Uptime is $result.");
-	}
+    return $result;
 }
 
-sub CheckSnmpServiceMetric ($$) {
-	my %Snmp_service_instance = %{shift @_};
-	my %Snmp_service_metric = %{shift @_};
-	my $result = 0;
+sub Check($$){
+    my %SNMP_service_instance = %{shift @_};
+    my %SNMP_service_metric = %{shift @_};
+    my $result = 0;
 
-	Errors("OK", "Check metric $Snmp_service_metric{'name'} for instance $Snmp_service_instance{'name'}");
+    if (!-f $METRICS_EXTRACTOR){
+       Errors("ERROR", "Could not find $METRICS_EXTRACTOR.");
+    }
 
-	$result = (split(/=/,`$METRICS_EXTRACTOR $Snmp_service_instance{'host'} -s $Snmp_service_metric{'name'}`))[1];
+    my $uptime = (split(/=/,`$METRICS_EXTRACTOR $SNMP_service_instance{'host'} -s TimeSec`))[1];
+    $uptime = 0 if (!$uptime or "" eq $uptime);
+    $uptime =~ s/^\s*//;
+    $uptime =~ s/\s*$//;
 
-	if (!$result or "" eq $result ){
-		SendSNMPTRAP("Snmp service instance $Snmp_service_instance{'name'} ($Snmp_service_instance{'host'}) is unavailable.");
-	} else {
-		$result =~ s/^\s*//;
-		$result =~ s/\s*$//;
+    if ($uptime eq "0"){
+       SendSNMPTRAP("error", "0", "SNMP service instance $SNMP_service_instance{'name'} ($SNMP_service_instance{'host'}) is unavailable.");
+       Errors("OK", "Write to hitory file ($HIST_FILE): $SNMP_service_instance{'name'}:TimeSec");
+       open (OUT_FILE, ">>$HIST_FILE") or Errors("ERROR", "Unable to open history file $HIST_FILE");
+       print OUT_FILE "$SNMP_service_instance{'name'}:TimeSec\n";
+       close (OUT_FILE);
+    }else{
+       if (&isClearTrapNeeded("$SNMP_service_instance{'name'}:TimeSec") eq "1"){
+          SendSNMPTRAP("clear", "$uptime", "SNMP service instance $SNMP_service_instance{'name'} is working. Uptime is $result.");
+       }else{
+          $result = (split(/=/,`$METRICS_EXTRACTOR $SNMP_service_instance{'host'} -s $SNMP_service_metric{'name'}`))[1];
+          $result = 0 if (!$result or "" eq $result);
+          $result =~ s/^\s*//;
+          $result =~ s/\s*$//;
 
-		my ($threshold_high, $threshold_low) = split(/:/, $Snmp_service_metric{'value'});
-		$threshold_high = 9**9**9 if (!defined($threshold_high) || $threshold_high eq "");
-		$threshold_low = 0 if (!defined($threshold_low) || $threshold_low eq "");
+          my ($threshold_high, $threshold_low) = split(/:/, $SNMP_service_metric{'value'});
+          $threshold_high = 9**9**9 if (!defined($threshold_high) || $threshold_high eq "");
+          $threshold_low = 0 if (!defined($threshold_low) || $threshold_low eq "");
 
-		if($result <= $threshold_low){
-			SendSNMPTRAP("Snmp service instance $Snmp_service_instance{'name'} has a low value ($result) for $Snmp_service_metric{'name'}.");
-		}elsif(($result <= $threshold_high) && ($result > $threshold_low)){
-			Errors("OK", "Snmp service instance $Snmp_service_instance{'name'} has a normal value ($result) for $Snmp_service_metric{'name'}");
-		}elsif($result > $threshold_high){
-			SendSNMPTRAP("Snmp service instance $Snmp_service_instance{'name'} has a high value ($result) for $Snmp_service_metric{'name'}.");
-		}
-
-	}
+          if ($result < $threshold_low){
+             SendSNMPTRAP("error", "$uptime", "SNMP service instance $SNMP_service_instance{'name'} has a low value ($result) for $SNMP_service_metric{'name'}.");
+             Errors("OK", "Write to hitory file ($HIST_FILE): $SNMP_service_instance{'name'}:$SNMP_service_metric{'name'}");
+             open (OUT_FILE, ">>$HIST_FILE") or Errors("ERROR", "Unable to open history file $HIST_FILE");
+             print OUT_FILE "$SNMP_service_instance{'name'}:$SNMP_service_metric{'name'}\n";
+             close (OUT_FILE);
+          }elsif (($result < $threshold_high) && ($result >= $threshold_low)){
+             if (&isClearTrapNeeded("$SNMP_service_instance{'name'}:$SNMP_service_metric{'name'}") eq "1"){
+                SendSNMPTRAP("clear", "$uptime",  "Snmp service instance $SNMP_service_instance{'name'} has a normal value ($result) for $SNMP_service_metric{'name'}");
+             }else{
+                Errors("OK", "SNMP service instance $SNMP_service_instance{'name'} has a normal value ($result) for $SNMP_service_metric{'name'}");
+             }
+          }elsif($result > $threshold_high){
+             SendSNMPTRAP("error", "$uptime", "SNMP service instance $SNMP_service_instance{'name'} has a high value ($result) for $SNMP_service_metric{'name'}.");
+             Errors("OK", "Write to hitory file ($HIST_FILE): $SNMP_service_instance{'name'}:$SNMP_service_metric{'name'}");
+             open (OUT_FILE, ">>$HIST_FILE") or Errors("ERROR", "Unable to open history file $HIST_FILE");
+             print OUT_FILE "$SNMP_service_instance{'name'}:$SNMP_service_metric{'name'}\n";
+             close (OUT_FILE);
+          }
+       }
+    }
 }
 
-sub GetAlertFromMETRICS_EXTRACTORLog($){
-	my $alerts_out = shift @_;
-}
-
-sub SendSNMPTRAP ($) {
+sub SendSNMPTRAP ($$$) {
+	my $trapType = shift @_;
+    my $uptime = shift @_;
 	my $trapMessage = shift @_;
 	Errors("WARNING", $trapMessage);
 
 	my $ENT_OID = ".1.3.6.1.4.1.2013";
-	my $Snmp_service_uptime = "0"; #"OSLEE_DEF_TimeSec";
-	my $HEADER = "$Snmp_service_uptime $ENT_OID $ENT_OID.1 string \"ENT\" $ENT_OID.2 string \"SNMP service\" $ENT_OID.3 string \"Performance Monitoring\"";
+	my $HEADER = "";
+    if ($trapType eq "error"){
+       $HEADER = "$uptime $ENT_OID $ENT_OID.1 string \"ENT\" $ENT_OID.2 string \"SNMP Service\" $ENT_OID.3 string \"Performance Monitoring\" $ENT_OID.4 string \"Trap\"";
+    }elsif($trapType eq "clear"){
+       $HEADER = "$uptime $ENT_OID $ENT_OID.1 string \"ENT\" $ENT_OID.2 string \"SNMP Service\" $ENT_OID.3 string \"Performance Monitoring\" $ENT_OID.4 string \"Clear\"";
+    }
 	
 	if (!-f $Snmp{'SNMPTRAP_command'}){
 		Errors("ERROR", "Could not find $Snmp{'SNMPTRAP_command'}");
@@ -277,9 +315,8 @@ sub SendSNMPTRAP ($) {
 &CheckConfiguration();
 
 foreach my $Snmp_service_key (keys %Snmp_service){
-	&CheckSnmpserviceInstanceAvailability({name=>$Snmp_service_key,host=>$Snmp_service{$Snmp_service_key}});
 	foreach my $metrics_key (keys %Metrics){
-		&CheckSnmpServiceMetric({name=>$Snmp_service_key,host=>$Snmp_service{$Snmp_service_key}},{name=>$metrics_key,value=>$Metrics{$metrics_key}});
+		&Check({name=>$Snmp_service_key,host=>$Snmp_service{$Snmp_service_key}},{name=>$metrics_key,value=>$Metrics{$metrics_key}});
 	}
 }
 
